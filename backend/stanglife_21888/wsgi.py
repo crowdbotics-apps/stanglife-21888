788d71df447451ef1c9e160a9860df5b4875c6e6
Crowdbotics <team@crowdbotics.com>
2020-10-23 19:26:08 +0000

    Initial commit.

---
+++ backend/stanglife_21888/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for stanglife_21888 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stanglife_21888.settings')
+
+application = get_wsgi_application()
